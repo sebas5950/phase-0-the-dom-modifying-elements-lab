e27b20c59555a3a5e9ad2c4af4625b03a9475877
Alex Guanipatin <sebas5950@gmail.com>
2022-05-23 09:11:07 -0400

    commit tree 0f45ff9c39a614c9671e156b9dba6d391d61f177

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
+
+// const theId = document.getElementById('main');
+//     theId.remove()
+
+// const h1 = document.createElement('h1');
+
+// h1.setAttribute('id', 'victory');
+
+// h1.textContent = 'ALEX is the champion';
+
+// document.body.append(h1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Write your code here!
+const element = document.getElementById("main");
+    element.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "Alex is the champion";
+
+document.body.append(newHeader);
+
